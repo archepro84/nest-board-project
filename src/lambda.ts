bf21f6b3858e88c191d24a670257d03da46b8b30
archepro84 <archepro84@gmail.com>
2022-02-12 23:39:39 +0900

    feat: Add dotenv package from main

--- src/lambda.ts
+++ src/lambda.ts
@@ -8,10 +8,22 @@ import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 import * as express from 'express';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
 
 const binaryMimeTypes: string[] = [];
 
 let cachedServer: Server;
+
+dotenv.config({
+  path: path.resolve(
+    process.env.NODE_ENV === 'production'
+      ? '.production.env'
+      : process.env.NODE_ENV === 'stage'
+      ? '.stage.env'
+      : '.development.env',
+  ),
+});
 
 async function bootstrapServer(): Promise<Server> {
   if (!cachedServer) {
