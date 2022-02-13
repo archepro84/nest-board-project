6545903743d2069a99934d105b08d674edd7f37b
archepro84 <archepro84@gmail.com>
2022-02-13 22:19:17 +0900

    feat: Remove dotenv from main logic

--- src/lambda.ts
+++ src/lambda.ts
@@ -8,22 +8,10 @@ import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 import * as express from 'express';
-import * as dotenv from 'dotenv';
-import * as path from 'path';
 
 const binaryMimeTypes: string[] = [];
 
 let cachedServer: Server;
-
-dotenv.config({
-  path: path.resolve(
-    process.env.NODE_ENV === 'production'
-      ? '.production.env'
-      : process.env.NODE_ENV === 'stage'
-      ? '.stage.env'
-      : '.development.env',
-  ),
-});
 
 async function bootstrapServer(): Promise<Server> {
   if (!cachedServer) {
