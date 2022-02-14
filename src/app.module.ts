ba4dbae0a8a54cb26410b5802172fa2652797218
archepro84 <archepro84@gmail.com>
2022-02-14 22:22:09 +0900

    feat: Add email module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,14 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { EmailModule } from './email/email.module';
 import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
+import { validationSchema } from './config/validationSchema';
+import emailConfig from './config/emailConfig';
 
 const configModuleOption: ConfigModuleOptions = {
-  envFilePath:
-    process.env.NODE_ENV === 'production'
-      ? '.production.env'
-      : process.env.NODE_ENV === 'stage'
-      ? '.stage.env'
-      : '.development.env',
+  envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+  load: [emailConfig],
+  isGlobal: true,
+  validationSchema,
 };
 
 @Module({
