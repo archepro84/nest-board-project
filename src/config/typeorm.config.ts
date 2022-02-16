ec13c1fa3ddd30d53cc0da3f52bb58f454666dd5
archepro84 <archepro84@gmail.com>
2022-02-16 23:36:05 +0900

    feat: Add config, env file

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeORMConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_SCHEMA,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: Boolean(process.env.DB_SYNCHRONIZE),
+};
