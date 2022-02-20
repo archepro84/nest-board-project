55a9f4832820b6a857a5f805c0c5b905651cc0a8
archepro84 <archepro84@gmail.com>
2022-02-20 18:03:07 +0900

    feat: Fix serverless framework default environment file Error

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,6 @@ import {
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { EmailModule } from './email/email.module';
 import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
 import { validationSchema } from './config/validationSchema';
 import emailConfig from './config/emailConfig';
@@ -21,7 +20,11 @@ import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './auth/auth.guard';
 
 const configModuleOption: ConfigModuleOptions = {
-  envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+  envFilePath: [
+    `${__dirname}/config/env/.${
+      process.env.NODE_ENV ? process.env.NODE_ENV : 'development'
+    }.env`,
+  ],
   load: [emailConfig],
   isGlobal: true,
   validationSchema,
