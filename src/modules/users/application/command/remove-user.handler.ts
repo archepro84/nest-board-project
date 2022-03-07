39744520a7bcd0bfe3c07b55c73d59d298b264e7
archepro84 <archepro84@gmail.com>
2022-03-07 22:06:04 +0900

    feat: Add remove user command template

---
+++ src/modules/users/application/command/remove-user.handler.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { RemoveUserCommand } from './remove-user.command';
+import { CommandHandler, EventBus, ICommandHandler } from '@nestjs/cqrs';
+import { Connection, Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from '../../entities/user.entity';
+
+@Injectable()
+@CommandHandler(RemoveUserCommand)
+export class RemoveUserHandler implements ICommandHandler<RemoveUserCommand> {
+  constructor(
+    private connection: Connection,
+    private eventBus: EventBus,
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
+  ) {}
+
+  async execute(command: RemoveUserCommand) {}
+}
